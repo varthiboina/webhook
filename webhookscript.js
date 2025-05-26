2245be422b90a73e8cb0c0536e2337d59c5f08e8
varthiboina <dhanushvarthiboina@gmail.com>
2025-05-26 20:56:17 +0000

    webhook commit

---
+++ webhookscript.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get('/github-webhook/hello' , (req,res) =>
+{
+    res.status(200).json({ message: "Hello, world from webhook git retrotale" });
+}
+);
+app.post('/github-webhook', (req, res) => {
+  const branch = req.body.ref;
+  if (branch === 'refs/heads/main') {
+    console.log('Push to main detected');
+
+    exec('cd /home/ec2-user/your-repo/sample && git pull', (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error: ${err.message}`);
+        return res.sendStatus(500);
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+      res.sendStatus(200);
+    });
+  } else {
+    console.log(`Push to branch: ${branch}`);
+    res.sendStatus(200);
+  }
+});
+
+app.listen(3002, () => {
+  console.log('Listening for GitHub webhook on port 3000');
+});
